6986a1b645a4c6b1726f093012cfef7fa34baec2
JustCasuallyJames <50897551+JustCasuallyJames@users.noreply.github.com>
2020-04-19 20:17:29 -0700

    Update register.js

--- routes/register.js
+++ routes/register.js
@@ -5,27 +5,27 @@ const User = require('../models/user.model');// this parts gotta go
 //gets every single user in the database
 router.get('/', async (req,res) => {
     try{
-        const posts = await User.find();
-        res.json(posts);
+        const user = await User.find();
+        res.json(user);
     }catch(err){
         res.send(err);
     }
 });
 
 //finds a user by their username.
-router.get('/:postId', async (req,res) =>{
-    const post = await User.findById(req.body.postId);
+router.get('/:userId', async (req,res) =>{
+    const id = await User.findById(req.body.userId);
     res.json(post);
 });
 
 router.post('/', async (req,res) =>{
-    const post = new User({
+    const user = new User({
         fullName: req.body.fullName,
         password: req.body.password,
         email: req.body.email
     });
-    const savedPost = await post.save(); //this returns a promise
-    res.json(savedPost);
+    const savedUser = await user.save(); //this returns a promise
+    res.json(savedUser);
 });
 
 module.exports = router;
