156edb764146ded997c271f25e1b0bc55943e04a
petre24 <pm_quin_24@yahoo.com>
2020-04-20 11:58:37 -0700

    added route descriptions; removed redundant route; included user authentication

--- routes/login.js
+++ routes/login.js
@@ -1,31 +1,54 @@
-const express = require('express');
+const express = require("express");
+const bcrypt = require("bcrypt");
 const router = express.Router();
-const User = require('../models/user.model');// this parts gotta go
+const User = require("../models/user.model"); // this parts gotta go
 
-//gets every single user in the database
-router.get('/', async (req,res) => {
-    try{
-        const user = await User.find();
-        res.json(user);
-    }catch(err){
-        res.send(err);
-    }
+// @route  GET login
+// @desc   gets all users
+// @access Public
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find().sort({ fullName: 1 });
+    res.json(users);
+  } catch (err) {
+    res.send(err);
+  }
 });
 
-//finds a user by their username.
-router.get('/:userId', async (req,res) =>{
-    const id = await User.findById(req.body.postId);
-    res.json(id);
-});
+/* the following post request is not needed as it is already in register.js and 
+    in users.js
+*/
+// //finds a user by their username.
+// router.get("/:userId", async (req, res) => {
+//   const id = await User.findById(req.body.userId);
+//   res.json(id);
+// });
 
-router.post('/', async (req,res) =>{
-    const post = new User({
-        fullName: req.body.fullName,
-        password: req.body.password,
-        email: req.body.email
-    });
-    const user = await post.save(); //this returns a promise
-    res.json(user);
+// @route POST login
+// @desc checks for user authentication
+// @access Public
+router.post("/", async (req, res) => {
+  await User.findOne({ fullName: req.body.fullName })
+    .then((user) => {
+      if (user == null) {
+        return res.status(400).json("Cannot find user");
+      }
+      try {
+        bcrypt.compare(req.body.password, user.password, function (
+          err,
+          result
+        ) {
+          if (result) {
+            res.send("Login Successful");
+          } else {
+            res.send("Login Unsuccessful");
+          }
+        });
+      } catch {
+        res.status(500).send();
+      }
+    })
+    .catch((err) => res.status(400).json(err));
 });
 
 module.exports = router;
