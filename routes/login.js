dba5f3605b8ec515c24c714e5a9b17ff848546fd
JustCasuallyJames <50897551+JustCasuallyJames@users.noreply.github.com>
2020-04-19 20:04:19 -0700

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const Post = require('../models/Post');// this parts gotta go
+const User = require('../models/user.model');// this parts gotta go
 
 //gets every single user in the database
 router.get('/', async (req,res) => {
     try{
-        const posts = await Post.find();
+        const posts = await User.find();
         res.json(posts);
     }catch(err){
         res.send(err);
@@ -14,13 +14,13 @@ router.get('/', async (req,res) => {
 
 //finds a user by their username.
 router.get('/:postId', async (req,res) =>{
-    const post = await Post.findById(req.body.postId);
+    const post = await User.findById(req.body.postId);
     res.json(post);
 });
 
 router.post('/', async (req,res) =>{
-    const post = new Post({
-        username: req.body.username,
+    const post = new User({
+        fullName: req.body.fullName,
         password: req.body.password,
         email: req.body.email
     });
