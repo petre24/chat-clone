fe4ec318eb7adab6564aea0b67b77ce25e79b049
Andrei Dimaano <56818175+andreidimaano@users.noreply.github.com>
2020-04-22 19:40:44 -0700

    Add files via upload

---
+++ login-user.component.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import axios from 'axios';
+
+class LoginUser extends Component {
+  constructor(){
+    super()
+    this.state = {
+        username: '',
+        password: '',
+    };
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChangeUsername = (e) => {
+    this.setState({
+      username: e.target.value,
+    });
+  };
+
+  onChangePassword = (e) => {
+    this.setState({
+      password: e.target.value,
+    });
+  };
+
+  onSubmit(e){
+    e.preventDefault()
+
+    const User = {
+      username: this.state.username,
+      password: this.state.password
+    };
+
+    axios.post("/login/", User).then((res) => console.log(res.data));
+
+    window.location = "/chat";
+  };
+
+ 
+  render() {
+    return(
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 mt-5 mx-auto">
+            <form noValidate onSubmit={this.onSubmit}>
+              <h1 className="h3 mb-3 font-weight-normal">Sign in or Bryson will Fire you</h1>
+              <div className="form-group">
+                <label htmlFor="username">Username</label>
+                <input 
+                  type="username"
+                  className="form-control"
+                  name="username"
+                  placeholder="Enter username"
+                  value={this.state.username}
+                  onChange={this.onChange}
+                  />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  name="password"
+                  placeholder="Enter password"
+                  onChange={this.onChangePassword}
+                  />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-lg btn-primary btn-block"
+              >
+                Sign in
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default LoginUser;
