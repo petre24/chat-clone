9e3e941eec0006d931f55ca0d81997d1b112da55
braydenlwong <63383908+braydenlwong@users.noreply.github.com>
2020-04-24 15:11:16 -0700

    made the home-Page absolutely stunning and drop dead goregious pretty

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css"
 
 import RegisterUser from "./components/register-user.component";
 import LoginUser from "./components/login-user.component";
--- client/src/components/home-page.component.js
+++ client/src/components/home-page.component.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
+import "./home-Page/home-Page/main.css";
 import toplogo from "./home-Page/home-Page/Images/TopLogo.jpg";
 import SignUpButton from "./home-Page/home-Page/Images/SignUpButton.jpg";
-import bottomLogo from "./home-Page/home-Page/Images/BottomLogo.jpg";
+import bottomLogo from "./home-Page/home-Page/Images/bottomLogo.png";
 import scrollimg from "./home-Page/home-Page/Images/ScrollDown1.jpg";
 import mailbox from "./home-Page/home-Page/Images/mailbox1.jpg";
 import bottom from "./home-Page/home-Page/Images/anotherthing_penis.png";
@@ -17,14 +18,14 @@ class HomePage extends Component {
               <nav>
                 <div id="outer-menu">
                   <div className="table">
-                    <ul id="h rizontal-list">
+                    <ul id="horizontal-list">
                       <li>
-                        <a href="login" style={{ color: "black" }}>
+                        <a href="login" style={{ color: "black", fontSize:28 }}>
                           Login
                         </a>
                       </li>
                       <li>
-                        <a href="register" style={{ color: "black" }}>
+                        <a href="register" style={{ color: "black", fontSize:28  }}>
                           Sign up
                         </a>
                       </li>
@@ -39,9 +40,9 @@ class HomePage extends Component {
           <div className="wrapper">
             <br></br>
             <br></br>
-            <img src={mailbox} alt="mainMailbox" className="center" />
+            <img src={mailbox} alt="mainMailbox" className="fml"  />
             <div className="img-with-text">
-              <h1 className="subtext">Messaging made Quick and Easy</h1>
+              <strong><h1 className="msg">Messaging made Quick and Easy</h1></strong>
               <h3 className="subtext">A Webservice for Private Messaging</h3>
               <img className="subimg" src={scrollimg} alt="scroll button" />
             </div>
@@ -50,23 +51,25 @@ class HomePage extends Component {
           <div className="grey-wrapper">
             <div className="wrapper">
               <strong>
-                <h1 className="signup-text">Create a free account now!</h1>
+                <h1 className="signup-text" className="botSignup">Create a free account now!</h1>
               </strong>
             </div>
             <div className="wrapper">
               <div className="SignUp-button">
-                <img
+                <a href="register">
+                  <img
                   src={SignUpButton}
                   alt="Sign up button"
                   width="20%"
-                  style={{ marginleft: "495" }}
-                />
+                  className="BootyholeMonday"
+                  />
+                </a>
               </div>
             </div>
           </div>
           <div className="dark-wrapper">
             <div className="wrapper">
-              <img src={bottomLogo} alt="bottom logo" width="15%" />
+              <img src={bottomLogo} alt="bottom logo" width="25%" className="botButtHole" />
             </div>
           </div>
         </div>
