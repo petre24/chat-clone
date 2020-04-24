fea3b8b3df81a0ffe331f065cd7f12bca1982d83
JustCasuallyJames <50897551+JustCasuallyJames@users.noreply.github.com>
2020-04-24 03:34:57 -0700

    Partial workings of chatbox (#20)
    -Partially got the chatbox to work but could not figure out how to send out messages
    -Need to figure out how to save all the messages to output to the screen
    
    Co-authored-by: James <jamesvo801@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -7,17 +7,58 @@ import LoginUser from "./components/login-user.component";
 import HomePage from "./components/home-page.component";
 import ChatScreen from "./components/chat-screen.component";
 
-function App() {
-  return (
-    <Router>
-      <div className="container">
-        <Route path="/" exact component={HomePage} />
-        <Route path="/register" component={RegisterUser} />
-        <Route path="/login" component={LoginUser} />
-        <Route path="/chat" component={ChatScreen} />
-      </div>
-    </Router>
-  );
+import axios from 'axios';
+import Pusher from 'pusher-js';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      chats: []
+    };
+
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+  componentDidMount() {
+    const pusher = new Pusher('db8ca11b6e7ab4c94470', {
+      cluster: 'mt1',
+      encrypted: true
+    });
+    const channel = pusher.subscribe('chat-clone');
+    channel.bind('message', data => {
+      this.setState({ chats: [...this.state.chats, data], test: '' });
+    });
+
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+
+  handleTextChange(e) {
+    if (e.keyCode === 13) { //if you press enter
+      console.log("We are pressing enter");
+      const payload = {
+        message: this.state.text //paper
+      };
+      axios.post('/message', payload); //is being called everytime when the texts in the box is changing
+    } else {
+      this.setState({ text: e.target.value });
+    }
+  }
+
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <Route path="/" exact component={HomePage} />
+          <Route path="/register" component={RegisterUser} />
+          <Route path="/login" component={LoginUser} />
+          <Route path="/chat"
+            render={(props) => <ChatScreen {...props} text={this.state.text} handleTextChange={this.handleTextChange} />} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
